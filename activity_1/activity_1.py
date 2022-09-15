9695192970b81d3dee0a68facdb8b08d3b625a47
Pepe <jo.ve.or.84@gmail.com>
2022-09-14 19:13:56 -0500

    Activity 1

---
+++ activity_1/activity_1.py
@@ -0,0 +1,9 @@
+import time
+
+user_name = input("What is your name?: ")
+time.sleep(1)
+print(f"Hello {user_name}!" )
+time.sleep(1)
+favorite_sport = input("What is your favorite sport?: ")
+time.sleep(2)
+print(f"Your favorite sport is {favorite_sport}!")
